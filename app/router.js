0a17b6465f30abbd6801b1da1abdf141141d9f6e
Robin Böning <rb@magiclabs.de>
2016-01-18 23:19:02 +0100

    Add cashbox resource (very basic implementation)

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('cashboxes', function() {
+    this.route('index', { path: "/" });
+  });
 });
 
 export default Router;
