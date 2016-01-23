4afc157094d7111501af9f41cfb8e7153961a76b
Robin Böning <rb@magiclabs.de>
2016-01-23 13:41:05 +0300

    Make JSONAPIAdapter and -Serializer to handle underscores ..instead of dashes in attributes as well as relations Further information, see this: https://github.com/rails-api/active_model_serializers/issues/974

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
-export default DS.ActiveModelAdapter.extend({
-  host: 'http://localhost:3000'
+export default DS.JSONAPIAdapter.extend({
+  host: 'http://localhost:3000',
+
+  pathForType: function(type) {
+    let underscored = Ember.String.underscore(type);
+    return Ember.String.pluralize(underscored);
+  }
 });
