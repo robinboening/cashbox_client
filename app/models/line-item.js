83ddf0dc5d0772bc8f305f32ebb18f47667c3410
Robin Böning <rb@magiclabs.de>
2016-02-17 01:33:34 +0100

    Add matter attribute to LineItem

--- app/models/line-item.js
+++ app/models/line-item.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   book:        DS.belongsTo(),
+  matter:      DS.attr('string'),
   receipt_url: DS.attr('string'),
   charged_at:  DS.attr('date'),
   amount:      DS.attr('number'),
